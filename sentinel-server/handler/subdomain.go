437488c44338da5091d00690d73746786d72f26a
0xgwyn <ali.dindamal@gmail.com>
2025-04-01 20:14:46 +0330

    Refactor AddSubdomains function

--- sentinel-server/handler/subdomain.go
+++ sentinel-server/handler/subdomain.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/0xgwyn/sentinel/database"
 	"github.com/0xgwyn/sentinel/models"
+	"github.com/dchest/validator"
 	"github.com/gofiber/fiber/v2"
 	sliceutil "github.com/projectdiscovery/utils/slice"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -41,67 +42,58 @@ func AddSubdomains(c *fiber.Ctx) error {
 		})
 	}
 
-	// Get all subdomains related to the given domain from the collection
-	filter := bson.M{"domain": domainName}
-	projection := bson.M{"name": 1, "_id": 0}
-	opts := options.Find().SetProjection(projection)
-	cursor, err := coll.Find(c.Context(), filter, opts)
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-		})
+	// Check if the subdomains are valid
+	for _, subdomain := range newSubdomains {
+		if !validator.IsValidDomain(subdomain) {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "invalid subdomain: " + subdomain,
+			})
+		}
 	}
-	defer cursor.Close(c.Context())
 
-	// Get the name of all subdomains inside the collection
-	oldSubdomains := []string{}
-	for cursor.Next(c.Context()) {
-		subdomain := models.Subdomain{}
-		err := cursor.Decode(&subdomain)
+	// Remove duplicates from the new subdomains
+	newUniqueSubdomains := sliceutil.Dedupe(newSubdomains)
+
+	// Prepare a list of subdomains to be added
+	subsToBeAdded := []models.Subdomain{}
+	for _, name := range newUniqueSubdomains {
+		// Check if the subdomain already exists in the database
+		filter := bson.M{"domain": domainName, "name": name}
+		count, err := coll.CountDocuments(c.Context(), filter)
 		if err != nil {
 			return c.Status(500).JSON(fiber.Map{
 				"error": err.Error(),
 			})
 		}
-		// only get the Name field
-		oldSubdomains = append(oldSubdomains, subdomain.Name)
+
+		// If the subdomain does not exist, add it to the list
+		if count == 0 {
+			subdomain := models.Subdomain{
+				Domain:    strings.ToLower(domainName),
+				Name:      strings.ToLower(name),
+				CreatedAt: bson.NewDateTimeFromTime(time.Now()),
+				UpdatedAt: bson.NewDateTimeFromTime(time.Now()),
+			}
+			subsToBeAdded = append(subsToBeAdded, subdomain)
+		}
 	}
 
-	// Trim spaces from all subdomains
-	for i := range newSubdomains {
-		newSubdomains[i] = strings.TrimSpace(newSubdomains[i])
-	}
-
-	// Make the new subdomains unique then compare old ones(in DB) with
-	// the new ones(in the request) and if no subdomain is new, let the
-	// user know
-	newUniqueSubdomains := sliceutil.Dedupe(newSubdomains)
-	_, subsToBeAdded := sliceutil.Diff(oldSubdomains, newUniqueSubdomains)
-	if subsToBeAdded == nil {
+	// If no new subdomains are found, return a message
+	if len(subsToBeAdded) == 0 {
 		return c.Status(200).JSON(fiber.Map{
 			"info": "no new subdomain was found",
 		})
 	}
 
-	// Create new subdomains then insert them to mongodb
-	subdomainsToInsert := []models.Subdomain{}
-	for _, name := range subsToBeAdded {
-		subdomain := models.Subdomain{
-			Domain:    strings.ToLower(domainName),
-			Name:      strings.ToLower(name),
-			CreatedAt: bson.NewDateTimeFromTime(time.Now()),
-			UpdatedAt: bson.NewDateTimeFromTime(time.Now()),
-		}
-		subdomainsToInsert = append(subdomainsToInsert, subdomain)
-	}
-
-	_, err = coll.InsertMany(c.Context(), subdomainsToInsert)
+	// Insert the new subdomains into the database
+	_, err := coll.InsertMany(c.Context(), subsToBeAdded)
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{
 			"error": err.Error(),
 		})
 	}
 
+	// Return the newly added subdomains
 	return c.Status(200).JSON(subsToBeAdded)
 }
 
